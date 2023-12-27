03fa50c2582f5692760bf7a4cd7370baf89f2447
のぶ <walk8243@gmail.com>
2023-12-27 16:13:17 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 cask "amethyst" do
-  version "{0}"
-  sha256 "{1}"
+  version "CASK_VERSION"
+  sha256 "CASK_SHA256"
 
   url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-mac.zip"
   name "Amethyst"
